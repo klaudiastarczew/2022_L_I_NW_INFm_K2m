2a92a8576ba1898cf08f7eb4ba4764374f84cf5f
klaudiastarczew <kstarczew@gmail.com>
2022-11-12 18:38:22 +0100

    Makefile added, noqa, lint analiza i poprawka

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,14 +6,16 @@ from flask import request
 moje_imie = "Klaudia"
 msg = "Hello World!"
 
-@app.route('/')
-def index():
-    output = request.args.get('output')
-    if not output:
-        output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
 
+@app.route('/')
+def index():    
+    output = request.args.get('output') 
+    if not output:  
+        output = PLAIN  
+    return get_formatted(msg, moje_imie,    
+                         output.lower())    
+
+    
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
