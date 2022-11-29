a35a520ecb607f0fdc1ea91f5286fb48535f2afd
Klaudia Starczew <kstarczew@gmail.com>
2022-11-29 21:57:33 +0100

    formatting fix 3

--- hello_world/views.py
+++ hello_world/views.py
@@ -8,14 +8,14 @@ msg = "Hello World!"
 
 
 @app.route('/')
-def index():    
-    output = request.args.get('output') 
-    if not output:  
-        output = PLAIN  
-    return get_formatted(msg, moje_imie,    
-                         output.lower())    
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, moje_imie,
+                         output.lower())
 
-    
+
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
